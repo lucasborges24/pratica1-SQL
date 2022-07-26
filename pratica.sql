362afdfa8e79442ba686f789ccc6040938829f58
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-26 19:15:26 -0300

    feat: add 3rd exercise answer

--- pratica.sql
+++ pratica.sql
@@ -12,3 +12,17 @@ ON testimonials."writerId" = writer."id"
 JOIN users recipient
 ON testimonials."recipientId" = recipient."id";
 
+-- Exercise 3
+SELECT users."id",
+users."name",
+courses."name" AS course,
+schools."name" AS school,
+educations."endDate"
+FROM users
+JOIN educations
+ON users."id" = 30
+JOIN courses
+ON educations."courseId" = courses."id"
+JOIN schools
+ON educations."schoolId" = schools."id"
+WHERE educations."status" = 'finished';
