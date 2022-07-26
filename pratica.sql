93b0f4b497a73039bd41e6d397c8bd30463b54b5
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-26 18:55:48 -0300

    feat: add exercise 2 answer

--- pratica.sql
+++ pratica.sql
@@ -4,3 +4,11 @@ FROM users
 JOIN cities
 ON users."cityId" = cities."id";
 
+-- Exercise 2
+SELECT writer."name" AS writer, recipient."name" AS recipient, testimonials."message"
+FROM testimonials
+JOIN users writer
+ON testimonials."writerId" = writer."id"
+JOIN users recipient
+ON testimonials."recipientId" = recipient."id";
+
